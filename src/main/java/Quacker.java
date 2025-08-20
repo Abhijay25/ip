25049c900aa81d986fb2161501c15ab49ce4e324
Abhijay <abhijay3852@gmail.com>
2025-08-21 00:22:54 +0800

    Edited Welcome Message

---
+++ src/main/java/Quacker.java
@@ -0,0 +1,11 @@
+public class Quacker {
+    public static void main(String[] args) {
+        String welcome = "----------------------------------- \n" //35 Dashes
+                + "Hello! I'm Quacker \n"
+                + "What can I do for you? \n"
+                + "----------------------------------- \n"
+                + "Bye. Hope to see you again soon!";
+
+        System.out.println(welcome);
+    }
+}
