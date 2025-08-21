cd085a61689b33bed5ef7dabd951b8a206069fcd
Abhijay <abhijay3852@gmail.com>
2025-08-21 15:27:32 +0800

    Added Marking and Unmarking Functionality

--- src/main/java/Quacker.java
+++ src/main/java/Quacker.java
@@ -7,15 +7,15 @@ public class Quacker {
     public static void main(String[] args) {
         // Initialize a scanner to take in prompt from the user
         Scanner scan = new Scanner(System.in);
-        List<String> toDo = new ArrayList<>();
+        List<Task> toDo = new ArrayList<>();
 
         String divider = "\n----------------------------------- \n"; //35 Dashes
 
         String welcome = divider
                         + "Hello! I'm Quacker \n"
-                        + "What can I do for you? \n"
+                        + "What can I do for you?"
                         + divider;
-        String goodbye = "Bye. Hope to see you again soon! \n";
+        String goodbye = "See you! Hopefully I see you again... *sad quack* \n";
         String prompt = "";
 
         System.out.println(welcome);
@@ -23,31 +23,83 @@ public class Quacker {
         while(prompt.equals("bye") == false) {
             System.out.println("Enter task: ");
             prompt = scan.nextLine();
+            prompt = prompt.toLowerCase(); // For case insensitive commands
 
             // If user asks for current list, display all
             if(prompt.equals("list")) {
                 int len = toDo.size();
 
                 if (len == 0) {
-                    String emptyResponse = divider + "Nothing to display" + divider;
+                    String emptyResponse = divider + "Nothing to display. Try giving me something to add!" + divider;
                     System.out.println(emptyResponse);
                 } else {
                     StringBuilder listResponse = new StringBuilder(divider);
                     for (int i = 0; i < len; i++) {
+                        Task item = toDo.get(i);
                         if(i == len - 1) {
-                            listResponse.append(i + 1).append(" ").append(toDo.get(i));
+                            listResponse.append(i + 1).append(". [").append(item.getStatusIcon()).append("] ").append(item.getDescription());
                         } else {
-                            listResponse.append(i + 1).append(" ").append(toDo.get(i)).append("\n");
+                            listResponse.append(i + 1).append(". [").append(item.getStatusIcon()).append("] ")
+                                    .append(item.getDescription()).append("\n");
                         }
                     }
                     listResponse.append(divider);
                     System.out.println(listResponse);
                 }
+            }
 
-            } else {
-                // Add prompt tolist and provide visual confirmation
-                toDo.add(prompt);
-                String response = divider + "added " + prompt + divider;
+            // Mark the task complete after checking for valid number
+            else if(prompt.startsWith("mark")) {
+                try {
+                    String taskString = prompt.substring(5); // To check whether we're given a valid number
+                    int taskNumber = Integer.parseInt(taskString);
+
+                    if (taskNumber > toDo.size()) {
+                        System.out.println(divider + "Please enter a valid task number!" + divider);
+                        continue;
+                    }
+
+                    // Once the number is valid, prepare the response message and mark as completed
+                    toDo.get(taskNumber - 1).setComplete();
+                    StringBuilder markResponse = new StringBuilder(divider);
+                    markResponse.append("'").append(toDo.get(taskNumber - 1).getDescription())
+                            .append("' has been marked completed!").append(divider);
+                    System.out.println(markResponse);
+
+                }
+                catch (NumberFormatException e){
+                    System.out.println(divider + "Please enter a valid task number!" + divider);
+                }
+            }
+
+            // Mark the task incomplete after checking for valid number
+            else if(prompt.startsWith("unmark")) {
+                try {
+                    String taskString = prompt.substring(7); // To check whether we're given a valid number
+                    int taskNumber = Integer.parseInt(taskString);
+
+                    if (taskNumber > toDo.size()) {
+                        System.out.println(divider + "Please enter a valid task number!" + divider);
+                        continue;
+                    }
+
+                    // Once the number is valid, prepare the response message and mark as completed
+                    toDo.get(taskNumber - 1).setIncomplete();
+                    StringBuilder markResponse = new StringBuilder(divider);
+                    markResponse.append("'").append(toDo.get(taskNumber - 1).getDescription())
+                            .append("' has been unmarked!").append(divider);
+                    System.out.println(markResponse);
+
+                }
+                catch (NumberFormatException e){
+                    System.out.println(divider + "Please enter a valid task number!" + divider);
+                }
+            }
+
+            // Add prompt toDo and provide visual confirmation
+            else {
+                toDo.add(new Task(prompt));
+                String response = divider + "'" + prompt + "' has been added to the list!" + divider;
                 System.out.println(response);
             }
         }
---
+++ src/main/java/Task.java
@@ -0,0 +1,28 @@
+public class Task{
+    protected String description;
+    protected boolean isCompleted;
+
+    public Task(String description) {
+        this.description = description;
+        this.isCompleted = false;
+    }
+
+    // Get the description of the task so far
+    public String getDescription() {
+        return this.description;
+    }
+
+    // Get the status icon to display for each task
+    public String getStatusIcon() {
+        return (this.isCompleted ? "X" : " ");
+    }
+
+    // Flip the completed status for task
+    public void setComplete() {
+        this.isCompleted = true;
+    }
+
+    public void setIncomplete() {
+        this.isCompleted = false;
+    }
+}
