b22697afde12c1f3804e23314f02dc58970082b5
Abhijay <abhijay3852@gmail.com>
2025-08-21 13:08:13 +0800

    Added List Functionality

--- src/main/java/Quacker.java
+++ src/main/java/Quacker.java
@@ -1,17 +1,15 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Quacker {
-
-//    public void promptUser() {
-//        System.out.println("Enter task: ");
-//        String prompt = scan.nextLine();
-//    }
 
     public static void main(String[] args) {
         // Initialize a scanner to take in prompt from the user
         Scanner scan = new Scanner(System.in);
+        List<String> toDo = new ArrayList<>();
 
-        String divider = "\n ----------------------------------- \n"; //35 Dashes
+        String divider = "\n----------------------------------- \n"; //35 Dashes
 
         String welcome = divider
                         + "Hello! I'm Quacker \n"
@@ -23,14 +21,35 @@ public class Quacker {
         System.out.println(welcome);
 
         while(prompt.equals("bye") == false) {
-            // Get user's prompt
             System.out.println("Enter task: ");
             prompt = scan.nextLine();
 
-            // If user says bye, end program
-            // Else continue asking for prompts
-            String response = divider + " " + prompt + divider;
-            System.out.println(response);
+            // If user asks for current list, display all
+            if(prompt.equals("list")) {
+                int len = toDo.size();
+
+                if (len == 0) {
+                    String emptyResponse = divider + "Nothing to display" + divider;
+                    System.out.println(emptyResponse);
+                } else {
+                    StringBuilder listResponse = new StringBuilder(divider);
+                    for (int i = 0; i < len; i++) {
+                        if(i == len - 1) {
+                            listResponse.append(i + 1).append(" ").append(toDo.get(i));
+                        } else {
+                            listResponse.append(i + 1).append(" ").append(toDo.get(i)).append("\n");
+                        }
+                    }
+                    listResponse.append(divider);
+                    System.out.println(listResponse);
+                }
+
+            } else {
+                // Add prompt tolist and provide visual confirmation
+                toDo.add(prompt);
+                String response = divider + "added " + prompt + divider;
+                System.out.println(response);
+            }
         }
 
         // Close scanner once user has said goodbye
